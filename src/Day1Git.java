4e87a213567dc0d2164402c28d6522b87dfffbf3
Metin Calik <metincalik2020@gmail.com>
2022-08-16 21:05:28 +0200

    BVC

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -2,5 +2,9 @@ public class Day1Git {
 
     public static void main(String[] args) {
 
+        System.out.println("hello git 1. Deneme ");
+        System.out.println("hello git 2. Deneme ");
+        System.out.println("hello git 3. Deneme ");
+
     }
 }
