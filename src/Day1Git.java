fb5b6575f9bacd95a65b50f9a0dabfb53b07494a
Metin Calik <metincalik2020@gmail.com>
2022-08-16 21:07:38 +0200

    cvc

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -5,6 +5,7 @@ public class Day1Git {
         System.out.println("hello git 1. Deneme ");
         System.out.println("hello git 2. Deneme ");
         System.out.println("hello git 3. Deneme ");
+        System.out.println("hello git 4. Deneme ");
 
     }
 }
